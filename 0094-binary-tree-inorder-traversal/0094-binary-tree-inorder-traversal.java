f3a1d974e248a45f39baffd873b5d5ed025ef9b8
nidhisebastian008 <nidcod@gmail.com>
2023-10-15 12:05:58 +0530

    Time: 1 ms (11.78%), Space: 40.5 MB (67.42%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if(root==null)return ans;
+        iterIn(root,ans);
+        return ans;
+    }
+    class TPair{
+        TreeNode node=null;
+        boolean selfdone=false;
+        boolean leftdone=false;
+        boolean rightdone=false;
+        TPair(TreeNode node, boolean selfdone,boolean leftdone, boolean rightdone){
+            this.node=node;
+            this.selfdone=selfdone;
+            this.leftdone=leftdone;
+            this.rightdone=rightdone;
+        }
+    }
+    public void iterIn(TreeNode root,List<Integer>ans) {
+        Stack<TPair> st = new Stack<>();
+        st.push(new TPair(root,false,false,false));
+        while(st.size()!=0)
+        {
+            if(st.peek().leftdone==false)
+            {
+                st.peek().leftdone=true;
+                if(st.peek().node.left!=null)st.push(new TPair(st.peek().node.left,false,false,false));
+            }
+            else if(st.peek().selfdone==false)
+            {
+                st.peek().selfdone=true;
+                ans.add(st.peek().node.val);
+            }
+            
+            else if(st.peek().rightdone==false)
+            {
+                st.peek().rightdone=true;
+                if(st.peek().node.right!=null)st.push(new TPair(st.peek().node.right,false,false,false));
+
+            }
+            else
+            {
+                st.pop();
+            }
+            
+        }
+
+        
+    }
+
+}
