9e3b8d4f2fe370699ae9cfbfa076ffc95003da2d
nidhisebastian008 <nidcod@gmail.com>
2023-10-18 20:55:33 +0530

    Time: 0 ms (100%), Space: 40.5 MB (67.44%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -15,53 +15,42 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        if(root==null)return ans;
-        iterIn(root,ans);
-        return ans;
-    }
-    class TPair{
-        TreeNode node=null;
-        boolean selfdone=false;
-        boolean leftdone=false;
-        boolean rightdone=false;
-        TPair(TreeNode node, boolean selfdone,boolean leftdone, boolean rightdone){
-            this.node=node;
-            this.selfdone=selfdone;
-            this.leftdone=leftdone;
-            this.rightdone=rightdone;
-        }
-    }
-    public void iterIn(TreeNode root,List<Integer>ans) {
-        Stack<TPair> st = new Stack<>();
-        st.push(new TPair(root,false,false,false));
-        while(st.size()!=0)
+        List<Integer> ans = new ArrayList<Integer>();
+        TreeNode curr=root;
+        while(curr!=null)
         {
-            if(st.peek().leftdone==false)
+            if(curr.left==null)
             {
-                st.peek().leftdone=true;
-                if(st.peek().node.left!=null)st.push(new TPair(st.peek().node.left,false,false,false));
-            }
-            else if(st.peek().selfdone==false)
-            {
-                st.peek().selfdone=true;
-                ans.add(st.peek().node.val);
-            }
-            
-            else if(st.peek().rightdone==false)
-            {
-                st.peek().rightdone=true;
-                if(st.peek().node.right!=null)st.push(new TPair(st.peek().node.right,false,false,false));
-
+             ans.add(curr.val);
+             curr=curr.right;  
             }
             else
             {
-                st.pop();
-            }
-            
-        }
+                TreeNode rightMost=rightMostOfLeftNode(curr);
+                if(rightMost.right==null)//thread create
+                {
+                    rightMost.right=curr;
+                    curr=curr.left;
+                }
+                else//thread break//left  khtm
+                {
+                    ans.add(curr.val);
+                    rightMost.right=null;
+                    curr=curr.right;
 
+                }
+            }
+        }
+        return ans;
         
     }
-
+    TreeNode rightMostOfLeftNode(TreeNode node)
+    {
+        TreeNode ans=node.left;
+        while(ans.right!=null&&ans.right!=node)
+        {
+            ans =ans.right;
+        }
+        return ans;
+    }
 }
